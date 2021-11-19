cbd3ac8fa7b2a27cbcf9c2e177ff2379a3ff4c1d
cabonaty0201 <90430157+cabonaty0201@users.noreply.github.com>
2021-11-19 14:12:57 -0300

    Add files via upload

---
+++ js/form.js
@@ -0,0 +1,4 @@
+document.getElementById('inputName'). value = "Esto es ";
+document.getElementById('inputAge'). value = "Un mensaje";
+document.getElementById('inputCity'). value = "De Prueba";
+document.getElementById('inputEmail4'). value = "mensajedeprueba@yahoo.com";
